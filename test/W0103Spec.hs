e03225210bd0f78ec5a22e683573e28e0726202e
Lars Brünjes <brunjlar@gmail.com>
2020-08-06 13:10:56 +0200

    fixed tests for w1.3

--- test/W0103Spec.hs
+++ test/W0103Spec.hs
@@ -1,19 +1,35 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module W0103Spec
     ( spec
     ) where
 
-import qualified Data.List as L
-import           Test.Hspec
-import           Test.QuickCheck
+import Test.Hspec
+import Test.QuickCheck
 
-import           W0101
+import W0103
 
 spec :: Spec
 spec = do
-    describe "permutations" $ do
-        it "generates all permutations" $ property prop_permutations
+    describe "splitleft'" $ do
+        it "behaves like splitleft" $ property prop_splitleft'
 
-prop_permutations :: [Int] -> Property
-prop_permutations xs =
-    let ys = take 6 xs -- only test lists up to length six
-    in  L.sort (permutations ys) === L.sort (L.permutations ys)
+prop_splitleft' :: Tree Int -> Property
+prop_splitleft' t = splitleft' t === splitleft t
+
+instance Arbitrary a => Arbitrary (Tree a) where
+
+    arbitrary = sized $ \s -> if s <= 1
+        then Leaf <$> arbitrary
+        else oneof [ Leaf <$> arbitrary
+                   , do
+                        sl <- choose (1, s - 1)
+                        let sr = s - sl
+                        Node <$> resize sl arbitrary <*> resize sr arbitrary
+                   ]
+
+    shrink (Leaf x)   = Leaf <$> shrink x
+    shrink (Node l r) = l :
+                        r :
+                        [Node l' r | l' <- shrink l] ++
+                        [Node l r' | r' <- shrink r]
